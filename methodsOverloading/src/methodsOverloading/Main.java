bc24f7be4c11c5f3c552730d62be29ca579e5173
Berkay Hasip Dural <110682297+berkayhasipdural@users.noreply.github.com>
2022-12-03 20:49:48 +0300

    MethodOverloading Notice(BTK Akademi)

---
+++ methodsOverloading/src/methodsOverloading/Main.java
@@ -0,0 +1,13 @@
+package methodsOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dortİslem dortIslem = new Dortİslem();
+		System.out.println(dortIslem.topla(3, 6));
+		dortIslem.topla(3, 4 , 5);
+		
+
+	}
+
+}
